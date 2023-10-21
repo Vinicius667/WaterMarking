93bd56cdabab7f99ac18c5d8fc041094973b37f0
Vinícius Almeida <almeidabarrosdf@gmail.com>
2023-10-21 07:54:17 -0300

    handle parameter incompatibility

--- face_watermark.py
+++ face_watermark.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw, ImageFont
 import os
 from glob import glob
-
+from typing import Union
 
 def rotate_image(img, angle):
     """
@@ -30,23 +30,24 @@ def make_grid(image, x_repeat, y_repeat):
     return aux
 
 
-def get_water_mark(image_shape: np.ndarray, watermark_text:str, font_size:int, occurrs: tuple, tilt_angle: float)-> np.ndarray:
+def get_water_mark(image_shape: np.ndarray, watermark_text:str, font_size:int, tilt_angle: float, occurrs : Union[None,tuple] = None, raise_on_incomp : bool = False)-> np.ndarray:
     """
     Retun watermark image. It does not apply the watermark to the original image because, in case it's used for a video, this water_mark can be a used multiple times without having to create it again for each frame.
     Params:
-        image: image to be watermarked
+        image_shape: shape of the image to be watermarked
         watermark_text: text to be used as watermark
         font_size: size of the font
-        occurrs: tuple with the number of times the watermark should be repeated in the x and y direction
+        occurrs: tuple with the number of times the watermark should be repeated in the x and y direction. This parameter can be omitted and the watermark will be repeated as many times as necessary to cover the entire image.
         tilt_angle: angle of the watermark
+        raise_on_incomp: if True, raise an error if the watermark cannot be repeated the number of times specified in occurrs. If False, the watermark will be repeated as many times as possible.
     """
 
     h_image, w_image, *_ = image_shape
     font = ImageFont.truetype("./COOPBL.TTF", font_size)
 
 
-    # Create a blank image with the same dimensions as the original image
-    image_text = Image.new('L', (image_shape[0],image_shape[1]), 0)
+    # Create a blank image with dimensions twice as large as the original image
+    image_text = Image.new('L', (w_image * 2, h_image * 2), 0)
 
     # Get a drawing context
     draw = ImageDraw.Draw(image_text)
@@ -65,25 +66,50 @@ def get_water_mark(image_shape: np.ndarray, watermark_text:str, font_size:int, o
     h_single_text, w_single_text = h_w_single_text
         
 
+    # If occurrs is not None, the watermark will be repeated the number of times specified in occurrs
     if occurrs:
+        # Times the watermark should be repeated in the x and y direction
         x_repeat, y_repeat = occurrs
-        x_margin_px = int((w_image/x_repeat - w_single_text)/2)
-        x_margin = np.zeros((h_single_text, x_margin_px))
-        image_text_np = np.concatenate((x_margin, image_text_np, x_margin), axis = 1)
 
+        # Add margins to the text image so that it can be repeated the number of times specified in occurrs
+        # Margins added to the sides of the text image
+        x_margin_px = int((w_image/x_repeat - w_single_text)/2)
+        if x_margin_px > 0:
+            x_margin = np.zeros((h_single_text, x_margin_px))
+            image_text_np = np.concatenate((x_margin, image_text_np, x_margin), axis = 1)
+        
+        # Margins added to the top and bottom of the text image
         h_single_text, w_single_text = image_text_np.shape
         y_margin_px = int((h_image/y_repeat - h_single_text)/2)
-        y_margin = np.zeros((y_margin_px, w_single_text))
-        image_text_np = np.concatenate((y_margin, image_text_np, y_margin), axis = 0)
+        if y_margin_px > 0:    
+            y_margin = np.zeros((y_margin_px, w_single_text))
+            image_text_np = np.concatenate((y_margin, image_text_np, y_margin), axis = 0)
 
+        # Make grid with the text image repeated the number of times specified in occurrs
         water_mark = make_grid(image_text_np, int(x_repeat), int(y_repeat))
         # Convert to uint8
         water_mark = water_mark.astype(np.uint8)
 
+
+        if min(image_shape[0] -  water_mark.shape[0], image_shape[1] - water_mark.shape[1]) < 0:
+            msg = "The number of repetitions specified in occurrs is not compatible with the combination of the other parameters."
+            if raise_on_incomp:
+                raise ValueError(msg)
+            else:
+                print(msg)
+                print("The watermark will be repeated as many times as possible.")
+
+                # Crop the image so that it has the same dimensions as the original image. May not lead to the desired result
+                water_mark = water_mark[:min(image_shape[0], water_mark.shape[0]), :min(image_shape[1], water_mark.shape[1])]
+
+
+        # Get difference between the dimensions of the original image and the watermark
         y_diff = image_shape[0] - water_mark.shape[0]
         x_diff = image_shape[1] - water_mark.shape[1]
 
 
+        
+        # Add margins to the watermark so that it has the same dimensions as the original image
         if y_diff:
             top_margin = np.zeros((y_diff//2, water_mark.shape[1]), dtype = np.uint8)
             bottom_margin = np.zeros((y_diff - y_diff//2, water_mark.shape[1]), dtype = np.uint8)
@@ -94,10 +120,12 @@ def get_water_mark(image_shape: np.ndarray, watermark_text:str, font_size:int, o
             right_margin = np.zeros((water_mark.shape[0], x_diff - x_diff//2), dtype = np.uint8)
             water_mark = np.concatenate((left_margin, water_mark, right_margin), axis = 1, dtype = np.uint8)
 
+    # If occurrs is None, the watermark will be repeated as many times as necessary to cover the entire image
     else:
         y_repeat, x_repeat = np.ceil(np.array((image_shape[0], image_shape[1]))/h_w_single_text)
         water_mark = make_grid(image_text_np, int(x_repeat), int(y_repeat))[:h_image, :w_image]
 
+    # Repeat the watermark in the 3 channels
     water_mark = np.dstack([water_mark for i in range(3)])
 
     return water_mark
@@ -134,7 +162,7 @@ if __name__ == "__main__":
             for transparency in [0.2]:
                 for tilt_angle in [30, 45]:
                     # Get watermark
-                    water_mark = get_water_mark(image.shape, watermark_text, font_size, occurrs, tilt_angle)
+                    water_mark = get_water_mark(image.shape, watermark_text, font_size, tilt_angle, occurrs)
 
                     # Add watermark to image
                     result_image = add_water_mark(image, water_mark, transparency)
@@ -160,7 +188,7 @@ if __name__ == "__main__":
     tilt_angle = 30
 
     # Get watermark. Because all the images have the same shape, we can use the same watermark for all of them
-    water_mark = get_water_mark((1080, 1920), watermark_text, font_size, occurrs, tilt_angle)
+    water_mark = get_water_mark((1080, 1920), watermark_text, font_size, tilt_angle, occurrs)
 
     for image_path in images:
         # Get basename
